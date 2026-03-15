60781b3cde65003e459b0f40173379a756e9f04d
luxus-0 <luxus0@gmail.com>
2026-03-15 23:27:28 +0100

    Remove unused class

--- app/domain/distance.py
+++
@@ -1,8 +0,0 @@
-class Distance:
-    meters: float
-
-    def __init__(self, meters):
-        self.meters = meters
-
-    def add(self, other: "Distance") -> "Distance":
-        return Distance(self.meters + other.meters)
